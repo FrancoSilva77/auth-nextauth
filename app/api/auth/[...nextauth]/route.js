cfe7af2a1bc1e85e7a653143ff8416901b3e002d
FrancoSilva77 <aspermasterone@gmail.com>
2024-07-18 08:00:46 -0600

    feat:Login Usuario-nextauth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth';
+import CredentialsProviders from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs'
+import db from '@/src/libs/db';
+
+const authOptions = {
+  providers: [
+    CredentialsProviders({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password', placeholder: '***' },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
+        const userFound = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!userFound) return null;
+
+        console.log(userFound);
+
+        const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
+
+        if(!matchPassword) return null
+
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email
+        }
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
